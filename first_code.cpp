8c1396d681e7a5cf38d4c323bb8cf4eee6413f21
WRD397 <writd97@gmail.com>
2022-05-09 21:55:53 +0530

    added

---
+++ first_code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
+
+// to run the program
+// 1. go to the very same directory
+// 2. g++ .\filename....this will create a software (suppose a.exe)
+// 3. .\a.exe
+// after pressing enter u will se the output
+
+// or install the extension Code Runner then simply press the un button available now at the top right corner.
