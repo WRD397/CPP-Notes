b665fb8bfa657df66eb22ba4db28544cd1c7410c
WRD397 <writd97@gmail.com>
2024-09-16 08:26:33 +0530

    new tests added.

--- draft_cpp.cpp
+++ draft_cpp.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
-void test()
-{
-	float score;
-	cout << "Please provide your score here: "<<endl;
-	cin >> score;
-	cout << "Your score as mentioned is as below." << endl << score;
+void test() {
+
+float score;
+cout << "Please provide your score here: "<<endl;
+cin >> score;
+cout << "Your score as mentioned is as below." << endl << score;
+cout << endl<<"Hello World!"<<endl;
 
 }
 
 
-void marks()
-{
-	int i;
-	float marks[6], sum = 0, avg;
+// void marks()
+// {
+// 	int i;
+// 	float marks[6], sum = 0, avg;
 	
-	cout << "Enter the marks of i-th student below" >> endl;
-	for(i=0; i<6; i++)
-	{	
-		cin >> 	marks[i] >> endl;
-	}
-	cout << "The marks you have entered are as belows..." << endl;
-	for (i=0; i<6; i++)
-	{
-		cout << to_string(marks[i]) << " ";		
-	}
+// 	cout << "Enter the marks of i-th student below" >> endl;
+// 	for(i=0; i<6; i++)
+// 	{	
+// 		cin >> 	marks[i] >> endl;
+// 	}
+// 	cout << "The marks you have entered are as belows..." << endl;
+// 	for (i=0; i<6; i++)
+// 	{
+// 		cout << to_string(marks[i]) << " ";		
+// 	}
 	
 	
-	// Calculating the average
+// 	// Calculating the average
 	
-	for (i=0; i <6; i++)
-	{
-		sum = sum + marks[i];
-	}
-	avg = sum / 6 ;
+// 	for (i=0; i <6; i++)
+// 	{
+// 		sum = sum + marks[i];
+// 	}
+// 	avg = sum / 6 ;
 	
-	cout endl << "and the averaeg is " << avg << endl;
-}
+// 	cout endl << "and the averaeg is " << avg << endl;
+// }
 
 
-int main()
-{
-char nl3[] = "\n\n\n";
-cout << endl;	
-cout << "Hey there!!"<<endl<<endl;
+int main() {
+// char nl3[] = "\n\n\n";
+// cout << endl;	
+// cout << "Hey there!!"<<endl<<endl;
 	
-	marks();
+test();
 
 
 
-cout << nl3;
+// cout << nl3;
 return 0;
 }
